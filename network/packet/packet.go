37fe256d8a8d48b102636c1a9eb6946717a7d4a5
huoshan017 <huoshan017@github.com>
2017-09-14 05:54:24 +0800

    mm

--- network/packet/packet.go
+++ network/packet/packet.go
@@ -1,7 +1,11 @@
 package network
 
+import (
+	"container/list"
+)
+
 type Packet interface {
 	Init(p interface{}) bool
-	Pack(data []byte) bool
-	Unpack(data []byte) bool
+	Pack(data []byte, packet_list *list.List) error
+	Unpack(data []byte) []byte
 }
